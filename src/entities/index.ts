ecacb10b853d1f2dbe85be45ac7b26d1e4db5fb8
gautamV19 <ee20b144@smail.iitm.ac.in>
2021-12-06 12:50:22 +0530

    all entities ready bug fix

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,4 +1,7 @@
+import Star from "./Common/Star";
+import NetwokingAndOpporunity from "./Netwoking_and_opporunity";
+import Comment from "./Common/Comment";
 import User from "./User";
 import UsersDev from "./UsersDev";
 
-export default [User, UsersDev];
+export default [User, UsersDev, NetwokingAndOpporunity, Comment, Star];
