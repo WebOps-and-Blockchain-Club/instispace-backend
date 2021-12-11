c24057c5842484d6f12ad99f85c754c40dd48726
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2021-12-11 14:49:59 +0530

    removed an extra console log from authchecker

--- src/utils/authcheker.ts
+++ src/utils/authcheker.ts
@@ -6,7 +6,6 @@ const authChecker: AuthChecker<MyContext> = async (
   { context: { user } },
   roles
 ) => {
-  console.log(user);
   if (!user) return false;
   if (roles.length === 0) return true;
   if (roles.includes(user.role)) return true;
