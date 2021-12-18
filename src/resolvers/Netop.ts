07b038d05eb111b57cabc5c01dcd4f914cd0e82e
gautamV19 <ee20b144@smail.iitm.ac.in>
2021-12-18 16:40:09 +0530

    filtering quqry

--- src/resolvers/Netop.ts
+++ src/resolvers/Netop.ts
@@ -178,7 +178,35 @@ class NetopResolver {
     }
   }
 
+  @Mutation(() => Boolean)
+  @Authorized()
+  async addComment(
+    @Arg("NetopId") netopId: string,
+    @Ctx() { user }: MyContext
+    @Arg("createCommentData") createCommentData:createCommentData
+  ) {
+    try {
+      const netop = await Netop.findOne(netopId, { relations: ["likedBy"] });
+      if (netop) {
+        if (netop.likedBy.includes(user)) {
+          netop.likedBy.filter((e) => e !== user);
+          netop.save();
+        } else {
+          netop.likedBy.push(user);
+          netop.save();
+        }
+        return netop!!;
+      } else {
+        throw new Error("Invalid netop id");
+      }
+    } catch (e) {
+      console.log(e.message);
+      throw new Error(e.message);
+    }
+  }
+
   @Query(() => Netop)
+  @Authorized()
   async getNetopById(@Arg("NetopId") netopId: string) {
     try {
       const netop = await Netop.findOne(netopId);
@@ -189,30 +217,59 @@ class NetopResolver {
     }
   }
 
-  // @Query(() => Netop)
-  // async getNetop(@Arg("NetopId") netopId: string) {
-  //   try {
-  //     const netop = await Netop.findOne(netopId);
-  //     return netop;
-  //   } catch (e) {
-  //     console.log(e.message);
-  //     throw new Error(e.message);
-  //   }
-  // }
+  @Query(() => [Netop])
+  @Authorized()
+  async getNetop(
+    @Arg("FileringCondition", { nullable: true }) fileringConditions: string[],
+    @Arg("SortingCondition", { nullable: true }) sortingCondition: string
+  ) {
+    try {
+      var netopList = await Netop.find({
+        where: { isHidden: false },
+        relations: ["tags"],
+      });
+      if (fileringConditions) {
+        var newList: Netop[] = [];
+        fileringConditions.forEach(async (tagName) => {
+          var tag = await Tag.findOne({ title: tagName });
+          if (tag) {
+            // still duplicates will be there
+            newList.concat(netopList.filter((e) => e.tags.includes(tag!)));
+          }
+        });
+        newList = arrayUnique(newList);
+        netopList = newList;
+      }
+      const MOSTLIKED = "mostLiked";
+      const MOSTRECENT = "mostRecent";
+      // const MOSTRELEVANT= "mostRelevant";
 
-  // @Query(() => [Netop])
-  // async getAllPosts() {
-  //   const posts = await Netop.find({});
-  //   return posts;
-  // }
+      switch (sortingCondition) {
+        case MOSTLIKED:
+          netopList.sort((a, b) => b.noOfLikes - a.noOfLikes);
+          break;
+        case MOSTRECENT:
+          // newList.sort((a, b) => b.time - a.time);
+          break;
+        default:
+          // mostRelavant
+          break;
+      }
 
-  // @Query(() => Comment)
-  // async getComment(@Arg("postId") postId: string) {
-  //   const Netop = await Netop.findOne(postId, {
-  //     relations: ["comments"],
-  //   });
-  //   return Netop?.comments;
-  // }
+      return netopList;
+    } catch (e) {
+      console.log(e.message);
+      throw new Error(e.message);
+    }
+  }
+
+  @Query(() => Comment)
+  async getComment(@Arg("netopId") netopId: string) {
+    const netop = await Netop.findOne(netopId, {
+      relations: ["comments"],
+    });
+    return netop?.comments;
+  }
 
   // @Mutation(() => Boolean)
   // async createPost(
@@ -244,3 +301,15 @@ class NetopResolver {
 }
 
 export default NetopResolver;
+
+//*** My Helpers */
+function arrayUnique(array: Netop[]): Netop[] {
+  var a = array.concat();
+  for (var i = 0; i < a.length; ++i) {
+    for (var j = i + 1; j < a.length; ++j) {
+      if (a[i] === a[j]) a.splice(j--, 1);
+    }
+  }
+
+  return a;
+}
