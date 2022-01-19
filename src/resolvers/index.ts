8ad64939ee1e31f6b23af2526d69ec40cd833a6f
gautamV19 <ee20b144@smail.iitm.ac.in>
2022-01-20 00:38:59 +0530

    subscription start

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -6,6 +6,7 @@ import AnnouncementResolver from "./Announcement";
 import NetopResolver from "./Netop";
 import CommentResolver from "./Comment";
 import ReportResolver from "./Report";
+import sampleRes from "./Subs";
 
 export default [
   UsersResolver,
@@ -15,5 +16,6 @@ export default [
   NetopResolver,
   CommentResolver,
   ReportResolver,
-  LostAndFoundResolver
+  LostAndFoundResolver,
+  sampleRes,
 ] as const;
