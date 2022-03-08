56ba1b190297955d3ef8a01b59a143204ce8cce7
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2022-03-08 19:37:32 +0530

    added query getSheetLink

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -39,6 +39,8 @@ export const accountPassword = "123456";
 
 export const smail = "@smail.iitm.ac.in";
 
+export const sheetLink = "https://docs.google.com/spreadsheets/d/1ECJ1V1KoJB7W3jNyrOD_F-46tKl_ZNPGRPNqMqZkNsA/edit?usp=sharing";
+
 export var salt = bcryptjs.genSaltSync(Number(process.env.ITERATIONS!));
 
 export function autoGenPass(length: number) {
