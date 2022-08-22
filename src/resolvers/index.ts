246d7a7941b27aa2e3014538c982c973746c27f2
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2022-08-22 14:34:31 +0530

    added locations resolver

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -14,6 +14,7 @@ import AmenitiesResolver from "./Amenities";
 import ContactResolver from "./Contacts";
 import ImageResolver from "./Image";
 import MessMenuResolver from "./MessMenu";
+import LocationsResolver from "./Locations";
 
 export default [
   UsersResolver,
@@ -31,5 +32,6 @@ export default [
   AmenitiesResolver,
   ContactResolver,
   ImageResolver,
+  LocationsResolver,
   MessMenuResolver,
 ] as const;
