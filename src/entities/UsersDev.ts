b83e140700154b95aa07275e6b05bf95ca6e1715
JanithMS <janithms9920@gmail.com>
2022-09-07 16:50:59 +0530

    Changed Dev Users list & Added displayName field

--- src/entities/UsersDev.ts
+++ src/entities/UsersDev.ts
@@ -5,8 +5,12 @@ import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
 @ObjectType("UsersDev", { description: "User's Dev List" })
 class UsersDev extends BaseEntity {
   @PrimaryColumn()
-  @Field({ description: "Roll-Number" })
+  @Field({ description: "Roll Number" })
   roll: string;
+
+  @Column()
+  @Field({ description: "LDAP Display Name" })
+  displayName: string;
 
   @Column()
   @Field({ description: "Password" })
