f81d2b03c046cf2caa64ec56ab9f6451b0b207e5
harsh <harshtrivs@gmail.com>
2023-06-23 23:35:11 +0530

    bug fix

--- src/notification/type/notification.input.ts
+++ src/notification/type/notification.input.ts
@@ -1,4 +1,5 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { UserRole } from 'src/user/type/role.enum';
 
 @InputType({ description: 'Input-Type for Create Notification Mutaion' })
 class NotificationInput {
@@ -13,6 +14,9 @@ class NotificationInput {
 
   @Field({ description: 'Body of Notification' })
   body: string;
+
+  @Field((_type) => [String], { description: 'Roles to be notified' })
+  roles: UserRole[];
 
   @Field({
     nullable: true,
